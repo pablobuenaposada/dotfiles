83cc70b3725d7b213481ac620d1d531cec7ef099
pablobuenaposada <pablo.buenaposada@gmail.com>
2025-07-23 22:19:58 +0200

    add phshrink

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,3 +1,4 @@
 # git commands
 alias gca='git commit --amend --no-edit'
 alias gpf='git push --force-with-lease origin HEAD'
+alias pishrink='docker run -it --rm --platform linux/amd64 --privileged=true -v \$(pwd):/workdir pishrink'
